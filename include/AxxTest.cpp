3cfa35d978fc4e49d10911ebb1f54d96d1bdc812
Richard Miles <richard.miles@fast-chat.co.uk>
2016-04-20 17:10:33 +0200

    Fix inter-test interference related to digitalWrite, analogWrite and pinMode

--- include/AxxTest.cpp
+++ include/AxxTest.cpp
@@ -4,15 +4,24 @@ namespace AxxTest{
 	
 	void initialiseArduinoMock(){
 
+		// Reset the Mock object
 		ArduinoHardwareMock.Reset();
 
 		ArduinoHardware = &ArduinoHardwareMock.get();
 		
+		// Recreate the default method responses
 		When(Method(ArduinoHardwareMock, pinMode)).AlwaysReturn();
 		When(Method(ArduinoHardwareMock, digitalRead)).AlwaysReturn(0);
 		When(Method(ArduinoHardwareMock, digitalWrite)).AlwaysReturn();
 		When(Method(ArduinoHardwareMock, analogRead)).AlwaysReturn(0);
 		When(Method(ArduinoHardwareMock, analogWrite)).AlwaysReturn();
 		When(Method(ArduinoHardwareMock, millis)).AlwaysReturn(0);
+		
+		// Reset the hardware variables - digitalWrite, analogWrite and pinMode
+		for(int pin = 0; pin < TEST_PIN_COUNT; pin++){
+			pin_modes[pin] = INPUT;
+			pin_digital_write[pin] = LOW;
+			pin_analog_write[pin] = LOW;
+		}
 	}
 }
